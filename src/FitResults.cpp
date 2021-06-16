b13109c350ad2d3da145ef666a0dbe9aafd64f84
anita.meraviglia <anita.meraviglia@mi.infn.it>
2021-06-16 09:57:11 +0200

    ToyDataGen updates

--- src/FitResults.cpp
+++ src/FitResults.cpp
@@ -52,9 +52,9 @@ auto NuFitResults::getCorrMatrix() -> std::vector<std::vector<double>> {
 // @brief Add results to existing NuFitResults. Used for ToyData fits.
 auto NuFitResults::addResults(NuFitResults results) -> NuFitResults {
 
-	//NuFitResults r(results.popt,results.pcov,results.efficiencies,results.errorflag,results.errorflag_cov,results.paramVector);
+	NuFitResults r(results.popt,results.pcov,results.efficiencies,results.errorflag,results.errorflag_cov,results.paramVector);
 
-	//return r;
+	return r;
 
 }
 
