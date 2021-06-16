12669e286a94ff9caf7b44b6e530ab0b8117b107
Alexandre Göttel <a.goettel@fz-juelich.de>
2021-06-16 22:32:46 +0200

    Fluctuate individual rates with poisson in toydatafits

--- src/ToyDataGenerator.cpp
+++ src/ToyDataGenerator.cpp
@@ -16,34 +16,34 @@
 
 namespace NuFitter{
 
-	//Temporary. It's the same function as in DataReader. Maybe there is an easier way to useit without
-	//re-write it here
-	auto getBinEdges_Toy(TH1D *hist, unsigned int nbins) -> std::vector<double> {
-		// Create bin_edges vector
-		std::vector<double> bin_edges;
-		for (auto i = 0U; i <= nbins; i++){
-			bin_edges.push_back(hist->GetBinLowEdge(i));
-		}
-		bin_edges.push_back(hist->GetBinLowEdge(nbins) +
-		                    hist->GetBinWidth(nbins));
-
-		return bin_edges;
+//Temporary. It's the same function as in DataReader. Maybe there is an easier way to useit without
+//re-write it here
+auto getBinEdges_Toy(TH1D *hist, unsigned int nbins) -> std::vector<double> {
+	// Create bin_edges vector
+	std::vector<double> bin_edges;
+	for (auto i = 0U; i <= nbins; i++){
+		bin_edges.push_back(hist->GetBinLowEdge(i));
 	}
+	bin_edges.push_back(hist->GetBinLowEdge(nbins) +
+	                    hist->GetBinWidth(nbins));
+
+	return bin_edges;
+}
+
 // template <class Config>
 auto generateToyData(const NuFitConfig config, const NuFitPDFs *pdfs) -> std::vector<NuFitData*> {
 
 	std::vector<NuFitData*> data;
 
 	for(auto t = 0U; t < config.ToyData; t++){
+		std::vector<TH1D*> hists;
+		std::vector<std::vector<double>> vec_data, bin_edges;
+		std::vector<unsigned int> hist_ids;
 
-	std::vector<TH1D*> hists;
-	std::vector<std::vector<double>> vec_data, bin_edges;
-	std::vector<unsigned int> hist_ids;
-
-	for (auto i = 1U; i <= config.data_hist_names.size(); i++) {
-		if (std::find(config.hist_id_toy.begin(), config.hist_id_toy.end(), i) != config.hist_id_toy.end()) {
+		for (auto i = 1U; i <= config.data_hist_names.size(); i++) {
+			if (std::find(config.hist_id_toy.begin(), config.hist_id_toy.end(), i) != config.hist_id_toy.end()) {
 				TH1D *hdata = new TH1D(config.data_hist_names[i-1].c_str(), config.data_hist_names[i-1].c_str(), config.nbins[i-1],
-				    pdfs->bin_edges[i-1].front() + 1, pdfs->bin_edges[i-1].back());
+				pdfs->bin_edges[i-1].front() + 1, pdfs->bin_edges[i-1].back());
 				hists.push_back(hdata);
 			}
 		}
@@ -51,33 +51,35 @@ auto generateToyData(const NuFitConfig config, const NuFitPDFs *pdfs) -> std::ve
 		gRandom = new TRandom3(0);
 		gRandom->SetSeed(0);
 
-		for (auto i = 0U; i < config.nSp_histos_toy.size(); i++){
-			for (auto k = 0U; k < config.nSp_histos_toy[i]; k++) {
+		// Fill histogram from PDFs
+		for (auto i = 0U; i < config.nSp_histos_toy.size(); i++) {  // For each toy hist
+			for (auto k = 0U; k < config.nSp_histos_toy[i]; k++) {// For each pdf in hist
 				auto idx = k + i * config.nSp_histos_toy[0];
 				auto current_hist = (TH1D*)pdfs->pdf_histograms[idx]->Clone();
-				hists.at(i)->FillRandom(current_hist, config.param_initial_guess_toy[idx]*config.param_eff_toy[idx]);
+				auto n_expected = config.param_initial_guess_toy[idx]*config.param_eff_toy[idx];
+				hists.at(i)->FillRandom(current_hist, gRandom->Poisson(n_expected));
 			}
 		}
+
 		// Convert histogram to vector
-			for (auto i = 1U; i <= config.data_hist_names.size(); i++) {
-				std::vector<double> vec_data_hist;
-				for (auto j = 0U; j <= config.nbins[i-1]; j++) {
-					vec_data_hist.push_back(hists.at(i-1)->GetBinContent(j));
-				}
-				// Create bin_edges vector
-				auto bin_edges_hist = getBinEdges_Toy(hists.at(i-1), config.nbins[i-1]);
-
-				bin_edges.push_back(bin_edges_hist);
-				vec_data.push_back(vec_data_hist);
-				hist_ids.push_back(i-1);
-
+		for (auto i = 1U; i <= config.data_hist_names.size(); i++) {
+			std::vector<double> vec_data_hist;
+			for (auto j = 0U; j <= config.nbins[i-1]; j++) {
+				vec_data_hist.push_back(hists.at(i-1)->GetBinContent(j));
 			}
+			// Create bin_edges vector
+			auto bin_edges_hist = getBinEdges_Toy(hists.at(i-1), config.nbins[i-1]);
 
+			bin_edges.push_back(bin_edges_hist);
+			vec_data.push_back(vec_data_hist);
+			hist_ids.push_back(i-1);
 
-	auto *data_tofill = new NuFitData(vec_data, bin_edges, hists, hist_ids);
-	data.push_back(data_tofill);
+		}
 
-}
+		auto *data_tofill = new NuFitData(vec_data, bin_edges, hists, hist_ids);
+		data.push_back(data_tofill);
+
+	}
 	return data;
 }
 }  // namespace NuFitter
